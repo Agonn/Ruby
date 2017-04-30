97cb81a9891cd5d86c3e615ef133f8d6163062d7
112 <yllbala@hotmail.com>
2017-04-30 23:29:10 +0200

    Login rita shkronjat + ndru fonti

--- Ruby/Properties/Login.Designer.cs
+++ Ruby/Properties/Login.Designer.cs
@@ -67,6 +67,8 @@
             // btnLogin
             // 
             this.btnLogin.BackColor = System.Drawing.Color.Transparent;
+            this.btnLogin.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.Black;
             this.btnLogin.Location = new System.Drawing.Point(345, 261);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(140, 23);
