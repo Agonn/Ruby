2d65c52a30969f6fc72361b6661b3b048a864c18
Agoni <Agon.r@msn.com>
2017-05-02 11:05:52 +0200

    Fixes

--- Ruby/MainForm.cs
+++ Ruby/MainForm.cs
@@ -28,7 +28,6 @@ namespace Ruby
         private void Form_Load(object sender, EventArgs e)
         {
             // TODO: This line of code loads data into the 'menaxhimiDataSet1.Malli' table. You can move, or remove it, as needed.
-            this.malliTableAdapter.Fill(this.menaxhimiDataSet1.Malli);
             // TODO: This line of code loads data into the 'menaxhimiDataSet1.Malli' table. You can move, or remove it, as needed.
             // TODO: This line of code loads data into the 'menaxhimiDataSet.Malli' table. You can move, or remove it, as needed.
 
@@ -50,9 +49,11 @@ namespace Ruby
         private void btnShto_Click_1(object sender, EventArgs e)
         {
 
-            SqlConnection objKonektimi = new SqlConnection(Parametrat._KonektimiDB);
+            SqlConnection objKonektimi = new SqlConnection();
+            objKonektimi.ConnectionString = "Data Source =.\\SQLEXPRESS;AttachDbFilename=|DataDirectory|\\Menaxhimi.mdf;Integrated Security = True; User Instance = True";
+
             //Formati DataTime DATETIME values in 'YYYY-MM-DD HH:MM:SS' 
-            string Query = "insert into dbo.tblMalli(Malli_Emri,Malli_Cmimi,Data_Shitjes,Pershkrimi) values('unaze','33.5','2017-05-02','eh')";
+            string Query = "insert into dbo.Malli(Malli_emri,Malli_cmimi,Data_shitjes,Pershkrimi) values('unaze','33.5','02/05/2017','eh')";
             SqlCommand objKomanda = new SqlCommand(Query, objKonektimi);
 
             try
