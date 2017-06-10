54b26a77bee806f48c9f4fc1b792aef671889238
112 <yllbala@hotmail.com>
2017-06-10 16:11:45 +0200

    test 2 per strper connectim

--- Ruby/Parametrat.cs
+++ Ruby/Parametrat.cs
@@ -1,11 +1,26 @@
 ﻿using System.Configuration;
+using System.Windows.Forms;
 
 namespace Ruby
 {
     class Parametrat
     {
 
-        public static string _KonektimiDB = ConfigurationManager.ConnectionStrings["KonektimiDB"].ToString();
 
+
+        static string folder = Application.ExecutablePath;
+
+
+        static string[] Potestoj = folder.Split('.');
+
+        static string test2 = Potestoj[0] + "DB.mdf";
+
+
+        //E bon stringun per konektim te gatshem per secilen Pc qe instalohet
+        static string strPerKonektimNew= "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" + test2+ ";Integrated Security=True;";
+
+       
+         static string OldStrPerConn= ConfigurationManager.ConnectionStrings["KonektimiDB"].ToString();
+        public static string _KonektimiDB = OldStrPerConn; 
     }
 }
