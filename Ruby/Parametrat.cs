79dd39c5b328adc92512b4b3b6f854c7fe53af6a
112 <yllbala@hotmail.com>
2017-06-10 12:37:24 +0200

    Komentim i kodit

--- Ruby/Parametrat.cs
+++ Ruby/Parametrat.cs
@@ -12,8 +12,9 @@ namespace Ruby
 
        static string[] data = folder.Split('\\');
 
+        //E bon stringun per konektim te gatshem per secilen Pc qe instalohet
        static string strPerKonektim = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" + data[0] + "\\" + data[1] + "\\" + data[2] + "\\" + data[3] + "\\" + data[4] + "\\" + data[5] + "\\" + data[6] + "\\RubyDB.mdf" + ";Integrated Security=True;";
 
-        public static string _KonektimiDB = strPerKonektim; //ConfigurationManager.ConnectionStrings["KonektimiDB"].ToString();
+        public static string _KonektimiDB = strPerKonektim; 
     }
 }
